7ca8df3f722fdabeb18a47dd27cb82adbec6c4b8
muhammad agil <rustofaiq06@gmail.com>
2025-05-23 13:10:21 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+let nama = prompt("Massukin nama lu");
+let umur = prompt("tulis umur lu");
+
+let konfirmasi = confirm("apakah nama sama umur lu udah bener??");
+alert("Nama lu " + nama + " dan umur lu " + umur);
+alert("ohh yaudah kalo bener");
